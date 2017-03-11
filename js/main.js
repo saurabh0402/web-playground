d192e7440832a7185ab20079a01f59f7423faee8
saurabh0402 <saurabh.k0402@gmail.com>
2017-03-11 10:32:54 +0530

    Allow filtering (Without animation)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 (function(){
-	
+	var games = document.getElementsByClassName("oneGame"),
+		menuButtons = document.getElementsByClassName("menuButton");
+
+	[].forEach.call(menuButtons, function(item){
+		item.addEventListener("click", function(e){
+
+			var activeMenu = document.getElementsByClassName("active")[0];
+			if(activeMenu !== this){
+				activeMenu.className = "menuButton inactive";
+				this.className = "menuButton active";
+
+				var category = this.dataset.category;
+
+				if(category === 'all'){
+					[].forEach.call(games, function(item){
+						item.className = "oneGame visible"
+					});
+				}
+
+				else {
+					[].forEach.call(games, function(item){
+						if(item.dataset.category === category)
+							item.className = "oneGame visible";
+						else
+							item.className = "oneGame hidden";
+					});
+				}
+			}
+
+		}, false);
+	});
 })();
